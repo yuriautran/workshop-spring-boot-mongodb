98ecbfaf96c0de7b7d486edaf4b07f6cea1ffd4f
Yuri Autran <ylautran@gmail.com>
2024-06-24 21:08:23 -0300

    EndPoint findById for User

---
+++ src/main/java/com/nelioalves/workshopmongo/resources/exceptions/StandardError.java
@@ -0,0 +1,58 @@
+package com.nelioalves.workshopmongo.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long timestamp;
+	private Integer status;
+	private String error;
+	private String path;
+	
+	public StandardError() {
+		
+	}
+
+	public StandardError(Long timestamp, Integer status, String error, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.path = path;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.nelioalves.workshopmongo.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +19,15 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		try {
+			Optional<User> user = repo.findById(id);
+			return user.get();
+		}
+		catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Objeto não encontrado para esse ID");
+		 }
 	}
 }
